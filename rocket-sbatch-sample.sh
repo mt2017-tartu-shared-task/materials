095871b946ede5e538bcbcb67529f9c822b2b661
Maksym Del <max.del.edu@gmail.com>
2017-09-19 01:51:34 +0300

    Add example SLURM sh file

---
+++ rocket-sbatch-sample.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#The name of the job is test_job
+#SBATCH -J rnnlm1_job
+
+#The job requires 1 compute node
+#SBATCH -N 1
+
+#The job requires 1 task per node
+#SBATCH --ntasks-per-node=1
+
+#The maximum walltime of the job is a 8 days
+#SBATCH -t 192:00:00
+
+#SBATCH --mem=50G
+
+#Leave this here if you need a GPU for your job
+#SBATCH --partition=gpu
+
+#SBATCH --gres=gpu:tesla:1
+
+#The module with all the NMT / deep learning packages
+module load python-2.7.13
+
+# PEOPLE HI, YOUR COMMANDS GO HERE
+
